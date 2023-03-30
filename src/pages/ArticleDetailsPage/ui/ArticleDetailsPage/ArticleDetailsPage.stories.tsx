5f9213b869d38e4a42add5c2457de741b34f5214
Anna Noskova <anni4anny@gmail.com>
2023-03-30 13:11:50 +0300

    add articles page

---
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.stories.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import ArticleDetailsPage from "./ArticleDetailsPage";
+
+export default {
+    title: "__/ArticleDetailsPage",
+    component: ArticleDetailsPage,
+    argTypes: {
+        backgroundColor: { control: "color" },
+    },
+} as ComponentMeta<typeof ArticleDetailsPage>;
+
+const Template: ComponentStory<typeof ArticleDetailsPage> = (args) => <ArticleDetailsPage {...args} />;
+
+export const Normal = Template.bind({});
+Normal.args = {};
---
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.stories.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import ArticlesPage from "./ArticlesPage";
+
+export default {
+    title: "__/ArticlesPage",
+    component: ArticlesPage,
+    argTypes: {
+        backgroundColor: { control: "color" },
+    },
+} as ComponentMeta<typeof ArticlesPage>;
+
+const Template: ComponentStory<typeof ArticlesPage> = (args) => <ArticlesPage {...args} />;
+
+export const Normal = Template.bind({});
+Normal.args = {};
